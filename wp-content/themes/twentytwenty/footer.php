65dcbab2e8345dbd52c3ed644777d86298410ba2
bulbulica2 <necrocris2000@yahoo.com>
2020-07-22 00:45:41 +0300

    More updates on themes

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -28,6 +28,12 @@
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 						</p><!-- .footer-copyright -->
 
+						<p class="powered-by-wordpress">
+							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
+								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+							</a>
+						</p><!-- .powered-by-wordpress -->
+
 					</div><!-- .footer-credits -->
 
 					<a class="to-the-top" href="#site-header">
